052263ed2a5472d19d266cba647c562854990c08
Jakub Rybinski <jakubrybinski@gmail.com>
2018-05-29 13:57:49 +0200

    Removed old entites and changelogs for luquibase

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,27 +1,22 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { AutomationServiceHubGuiRegisterModule } from './gui-register/gui-register.module';
-import { AutomationServiceHubNodeRegistryModule } from './node-registry/node-registry.module';
-import { AutomationServiceHubNodeMetaRegistryModule } from './node-meta-registry/node-meta-registry.module';
 import { AutomationServiceHubLibraryRegistryModule } from './library-registry/library-registry.module';
 import { AutomationServiceHubSourcesModule } from './sources/sources.module';
 
 import { AutomationServiceHubNodesModule } from './nodes/nodes.module';
 import { AutomationServiceHubGuisModule } from './guis/guis.module';
 import { AutomationServiceHubNodesMetaModule } from './nodes-meta/nodes-meta.module';
+
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
-        AutomationServiceHubGuiRegisterModule,
-        AutomationServiceHubNodeRegistryModule,
-        AutomationServiceHubNodeMetaRegistryModule,
         AutomationServiceHubLibraryRegistryModule,
         AutomationServiceHubSourcesModule,
         AutomationServiceHubSourcesModule,
         AutomationServiceHubNodesModule,
         AutomationServiceHubGuisModule,
-        AutomationServiceHubNodesMetaModule,
+        AutomationServiceHubNodesMetaModule
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
